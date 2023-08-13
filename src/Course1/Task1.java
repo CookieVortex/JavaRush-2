373a773b7beffe02798ebbc135435b354523c75e
Maxim <golub94@mail.ru>
2023-08-13 11:02:33 +0300

    Task #1

---
+++ src/Course1/Task1.java
@@ -0,0 +1,14 @@
+package Course1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Task1 {
+    public static void main(String[] args) {
+
+        int currentYear = 3126;
+        int year = 8;
+
+        System.out.println( currentYear - year );
+
+    }
+}
