eb89461caaec21e88873e69184aeb46e5e8396a4
Maxim <golub94@mail.ru>
2023-08-27 19:43:30 +0300

    Completed new tasks

---
+++ src/Course3/Task1.java
@@ -0,0 +1,9 @@
+package Course3;
+
+//В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.
+public class Task1 {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
