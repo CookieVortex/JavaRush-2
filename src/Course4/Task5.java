362cb18e4207e19136c344526ff4b4b7c47ddf6d
Maxim <golub94@mail.ru>
2023-09-12 19:27:30 +0300

    Completed new tasks

---
+++ src/Course4/Task5.java
@@ -0,0 +1,14 @@
+package Course4;
+//Как тебе уже известно, всего в языке Java 8 примитивных типов.
+//Создай по одной переменной каждого примитивного типа в классе Solution.
+//Переменные не должны находиться в каком-либо методе.
+public class Task5 {
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    boolean g;
+    char i;
+}
---
+++ src/Course4/Task6.java
@@ -0,0 +1,11 @@
+package Course4;
+//
+//Создай по одной переменной каждого примитивного целочисленного типа в классе Solution.
+  //      Этим переменным присвой максимально возможное для них значение.
+    //    Никаких методов создавать не нужно.
+public class Task6 {
+    byte a = Byte.MAX_VALUE;
+    short b = Short.MAX_VALUE;
+    int c = Integer.MAX_VALUE;
+    long d = Long.MAX_VALUE;
+}
