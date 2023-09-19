f434974bc45e4495af13d4517566ad4dbf7ee68f
Maxim <golub94@mail.ru>
2023-09-19 20:19:23 +0300

    Completed new tasks

---
+++ src/Course4/Task13.java
@@ -0,0 +1,21 @@
+package Course4;
+
+import java.util.Arrays;
+
+//Часто перед началом использования массива его необходимо заполнить значениями по умолчанию.
+//Реализуй такое заполнение в методе fillArray(Integer[] array, int value).
+//В качестве аргументов метод принимает массив и значение, которым его необходимо заполнить.
+//Массив заполняется полностью, независимо от его длины.
+public class Task13 {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value) {
+        for (int i = 1; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+}
